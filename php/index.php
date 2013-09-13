f7e7b58cb067aaf9634c2d6f091ad7cd2853b063
Nmcollins-cc <Nmcollins-cc@conestogac.on.ca>
2013-09-13 17:23:55 -0400

    Set up public version of calendar

--- php/index.php
+++ php/index.php
@@ -1,3 +1,7 @@
 <?php
 echo 'Nicholas Rocks!';
 ?>
+<iframe
+	src="https://www.google.com/calendar/embed?src=afnbvram6j1929ui1i6cqr4h1s%40group.calendar.google.com&ctz=America/Toronto"
+	style="border: 0" width="800" height="600" frameborder="0"
+	scrolling="no"></iframe>
