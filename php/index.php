ea1427233b58c680e354b8525b484affcf84f7b6
unknown <Nicholas@.(none)>
2014-05-21 17:41:01 -0400

    Reset back to original state

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Nicholas Rocks Test!';
+echo 'Nicholas Rocks!';
 ?>
 <iframe
 	src="https://www.google.com/calendar/embed?src=afnbvram6j1929ui1i6cqr4h1s%40group.calendar.google.com&ctz=America/Toronto"
